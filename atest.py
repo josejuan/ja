28bd5215b181b711061501e5c5eb5e8c57c34c02
josejuan <dev@computer-mind.com>
2023-08-17 16:40:58 +0200

    working example asr with gpu support

--- atest.py
+++ atest.py
@@ -1,11 +1,10 @@
+import torch
 import nemo
 import nemo.collections.asr as asr
 
-# asr.models.EncDecCTCModel.list_available_models()
-
 MODEL_NAME='stt_es_quartznet15x5'
 
-qn = asr.models.EncDecCTCModel.from_pretrained(model_name=MODEL_NAME)
-qn = qn.to('cuda')
+qn = asr.models.EncDecCTCModel.from_pretrained(model_name=MODEL_NAME) #.cuda()
+
 for t in qn.transcribe(paths2audio_files=['../wavs/complex.wav']):
   print(f">>>> {t}")
